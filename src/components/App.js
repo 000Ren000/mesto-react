96e5b4b8acbde563c081a29f8a46117709f4e3c8
000Ren000 <antonren@yandex.ru>
2022-06-14 11:45:29 +0700

    fix / children - этот атрибут появляется автоматически в компоненте

--- src/components/App.js
+++ src/components/App.js
@@ -31,88 +31,77 @@ function App() {
           onCardClick={setSelectedCard}
     />
      <Footer />
-    <PopupWithForm title="Редактировать профиль" name='edit-form' children={
-      <>
-      <input name="name"
-                 type="text"
-                 className="edit-form__input edit-form__input_type_name"
-                 id="input-profile-name"
-                 minLength="2"
-                 maxLength="40"
-                 required
-          />
-          <span className="popup__error" id="input-profile-name-error"></span>
-          <input name="about"
-                 type="text"
-                 className="edit-form__input edit-form__input_type_profession"
-                 id="input-profession"
-                 minLength="2"
-                 maxLength="200"
-                 required
-          />
-          <span className="popup__error" id="input-profession-error"></span>
-      </>
-    }
+    <PopupWithForm title="Редактировать профиль" name='edit-form'
                    buttonText="Сохранить"
                    isOpen={isEditProfilePopupOpen}
-                   onClose={closeAllPopups}
-    />
+                   onClose={closeAllPopups}>
+      <input name="name"
+             type="text"
+             className="edit-form__input edit-form__input_type_name"
+             id="input-profile-name"
+             minLength="2"
+             maxLength="40"
+             required
+      />
+      <span className="popup__error" id="input-profile-name-error"></span>
+      <input name="about"
+             type="text"
+             className="edit-form__input edit-form__input_type_profession"
+             id="input-profession"
+             minLength="2"
+             maxLength="200"
+             required
+      />
+      <span className="popup__error" id="input-profession-error"></span>
+    </PopupWithForm>
 
-     <PopupWithForm title="Новое место" name='add-Form' children={
-       <>
-       <input name="name"
-                id="input-name"
-                placeholder="Название"
-                type="text"
-                className="edit-form__input edit-form__input_type_name "
-                minLength="2"
-                maxLength="30"
-                required
-         />
-         <span className="popup__error" id="input-name-error"></span>
-         <input name="link"
-                id="input-link"
-                placeholder="Ссылка на картинку"
-                type="url"
-                className="edit-form__input edit-form__input_type_link"
-                required
-         />
-         <span className="popup__error" id="input-link-error"></span>
-       </>
-     }
+     <PopupWithForm title="Новое место" name='add-Form'
                     buttonText="Создать"
                     isOpen={isAddPlacePopupOpen}
-                    onClose={closeAllPopups}
-     />
+                    onClose={closeAllPopups}>
+       <input name="name"
+              id="input-name"
+              placeholder="Название"
+              type="text"
+              className="edit-form__input edit-form__input_type_name "
+              minLength="2"
+              maxLength="30"
+              required
+       />
+       <span className="popup__error" id="input-name-error"></span>
+       <input name="link"
+              id="input-link"
+              placeholder="Ссылка на картинку"
+              type="url"
+              className="edit-form__input edit-form__input_type_link"
+              required
+       />
+       <span className="popup__error" id="input-link-error"></span>
+     </PopupWithForm>
 
-     <PopupWithForm title="Обновить аватар" name='add-Form' children={
-       <>
-           <input name="link"
-                  id="input-avatar"
-                  placeholder="Ссылка на картинку"
-                  type="url"
-                  className="edit-form__input edit-form__input_type_link"
-                  required
-           />
-           <span className="popup__error" id="input-avatar-error"></span>
-         </>
-     }
+     <PopupWithForm title="Обновить аватар" name='add-Form'
                     buttonText="Сохранить"
                     isOpen={isEditAvatarPopupOpen}
-                    onClose={closeAllPopups}
-     />
-     <PopupWithForm title="Вы уверены?" name='add-Form' children={
-      <>
-         <button
-             type="submit"
-             className="edit-form__button-save"
-         >Да
-         </button
-         >
-       </>
-     }
-                    onClose={closeAllPopups}
-     />
+                    onClose={closeAllPopups}>
+       <input name="link"
+              id="input-avatar"
+              placeholder="Ссылка на картинку"
+              type="url"
+              className="edit-form__input edit-form__input_type_link"
+              required
+       />
+       <span className="popup__error" id="input-avatar-error"></span>
+     </PopupWithForm>
+
+     <PopupWithForm title="Вы уверены?" name='add-Form'
+                    onClose={closeAllPopups}>
+       <button
+           type="submit"
+           className="edit-form__button-save"
+       >Да
+       </button
+       >
+     </PopupWithForm>
 
      <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
 
