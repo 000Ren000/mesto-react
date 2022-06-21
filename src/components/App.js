a3a2f8be0b877f69a5cb22440f604a762df0ac57
000Ren000 <antonren@yandex.ru>
2022-06-21 14:10:14 +0700

    add / Создал объект контекста и используйте провайдер

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import ImagePopup from './ImagePopup.js';
 import {useState} from 'react';
 import {useEffect} from 'react';
 import {api} from '../utils/Api.js';
+import {CurrentUserContext} from '../contexts/CurrentUserContext.js'
 
 function App() {
 
@@ -35,85 +36,90 @@ function App() {
 		).catch(err => console.log('что-то пошло не так', err));
 	}, []);
 
-	return (<div className='root'>
-		<Header/>
+	return (
+			<CurrentUserContext.Provider value={currentUser}>
+				<div className='root'>
 
-		<Main onEditProfile={handleEditProfileClick}
-		      onAddPlace={handleAddPlaceClick}
-		      onEditAvatar={handleEditAvatarClick}
-		      onCardClick={setSelectedCard}
-		/>
-		<Footer/>
-		<PopupWithForm title="Редактировать профиль" name='edit-form'
-		               buttonText="Сохранить"
-		               isOpen={isEditProfilePopupOpen}
-		               onClose={closeAllPopups}>
-			<input name="name"
-			       type="text"
-			       className="edit-form__input edit-form__input_type_name"
-			       id="input-profile-name"
-			       minLength="2"
-			       maxLength="40"
-			       required
-			/>
-			<span className="popup__error" id="input-profile-name-error"></span>
-			<input name="about"
-			       type="text"
-			       className="edit-form__input edit-form__input_type_profession"
-			       id="input-profession"
-			       minLength="2"
-			       maxLength="200"
-			       required
-			/>
-			<span className="popup__error" id="input-profession-error"></span>
-		</PopupWithForm>
+					<Header/>
 
-		<PopupWithForm title="Новое место" name='add-Form'
-		               buttonText="Создать"
-		               isOpen={isAddPlacePopupOpen}
-		               onClose={closeAllPopups}>
-			<input name="name"
-			       id="input-name"
-			       placeholder="Название"
-			       type="text"
-			       className="edit-form__input edit-form__input_type_name "
-			       minLength="2"
-			       maxLength="30"
-			       required
-			/>
-			<span className="popup__error" id="input-name-error"></span>
-			<input name="link"
-			       id="input-link"
-			       placeholder="Ссылка на картинку"
-			       type="url"
-			       className="edit-form__input edit-form__input_type_link"
-			       required
-			/>
-			<span className="popup__error" id="input-link-error"></span>
-		</PopupWithForm>
+					<Main onEditProfile={handleEditProfileClick}
+					      onAddPlace={handleAddPlaceClick}
+					      onEditAvatar={handleEditAvatarClick}
+					      onCardClick={setSelectedCard}
+					/>
+					<Footer/>
+					<PopupWithForm title="Редактировать профиль" name='edit-form'
+					               buttonText="Сохранить"
+					               isOpen={isEditProfilePopupOpen}
+					               onClose={closeAllPopups}>
+						<input name="name"
+						       type="text"
+						       className="edit-form__input edit-form__input_type_name"
+						       id="input-profile-name"
+						       minLength="2"
+						       maxLength="40"
+						       required
+						/>
+						<span className="popup__error" id="input-profile-name-error"></span>
+						<input name="about"
+						       type="text"
+						       className="edit-form__input edit-form__input_type_profession"
+						       id="input-profession"
+						       minLength="2"
+						       maxLength="200"
+						       required
+						/>
+						<span className="popup__error" id="input-profession-error"></span>
+					</PopupWithForm>
 
-		<PopupWithForm title="Обновить аватар" name='add-Form'
-		               buttonText="Сохранить"
-		               isOpen={isEditAvatarPopupOpen}
-		               onClose={closeAllPopups}>
-			<input name="link"
-			       id="input-avatar"
-			       placeholder="Ссылка на картинку"
-			       type="url"
-			       className="edit-form__input edit-form__input_type_link"
-			       required
-			/>
-			<span className="popup__error" id="input-avatar-error"></span>
-		</PopupWithForm>
+					<PopupWithForm title="Новое место" name='add-Form'
+					               buttonText="Создать"
+					               isOpen={isAddPlacePopupOpen}
+					               onClose={closeAllPopups}>
+						<input name="name"
+						       id="input-name"
+						       placeholder="Название"
+						       type="text"
+						       className="edit-form__input edit-form__input_type_name "
+						       minLength="2"
+						       maxLength="30"
+						       required
+						/>
+						<span className="popup__error" id="input-name-error"></span>
+						<input name="link"
+						       id="input-link"
+						       placeholder="Ссылка на картинку"
+						       type="url"
+						       className="edit-form__input edit-form__input_type_link"
+						       required
+						/>
+						<span className="popup__error" id="input-link-error"></span>
+					</PopupWithForm>
 
-		<PopupWithForm title="Вы уверены?" name='add-Form'
-		               onClose={closeAllPopups}
-		               buttonText="Да"
-		/>
+					<PopupWithForm title="Обновить аватар" name='add-Form'
+					               buttonText="Сохранить"
+					               isOpen={isEditAvatarPopupOpen}
+					               onClose={closeAllPopups}>
+						<input name="link"
+						       id="input-avatar"
+						       placeholder="Ссылка на картинку"
+						       type="url"
+						       className="edit-form__input edit-form__input_type_link"
+						       required
+						/>
+						<span className="popup__error" id="input-avatar-error"></span>
+					</PopupWithForm>
 
-		<ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+					<PopupWithForm title="Вы уверены?" name='add-Form'
+					               onClose={closeAllPopups}
+					               buttonText="Да"
+					/>
 
-	</div>);
+					<ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+
+				</div>
+			</CurrentUserContext.Provider>
+	);
 }
 
 export default App;
